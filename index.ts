96bf43f1a7279fc8c36c7f5a2d91ba98a2c6f602
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-23 12:35:44 +0500

    Completed number guess

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+//1) computer will generate a random number --Done
+//2) user will guess the number 
+//3) compare user input with computer generated number and show result.
+
+const randomNumber=Math.floor(Math.random()*6+1);
+console.log(randomNumber);
+const answer=await inquirer.prompt([
+    {
+        name:"userGuessNumber",
+        type:"number",
+        message:"Please guess a number between 1 to 6",
+    },
+]);
+
+console.log(answer)
+
+if(answer.userGuessNumber===randomNumber){
+    console.log("Congratulation ! you guess the right number");
+
+}else{
+    console.log("You guess a wrong number");
+}
